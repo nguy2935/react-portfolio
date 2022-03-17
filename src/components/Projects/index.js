cc648de2239148d2723a0841ff2321e0e9a04523
unknown <Sophia Nguyen>
2022-03-17 02:00:54 -0500

    initial projects script file setup

---
+++ src/components/Projects/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <section>
+            <h1>Project 1</h1>
+            <p>Description about Project</p>
+        </section>
+    );
+}
+
+export default Projects;

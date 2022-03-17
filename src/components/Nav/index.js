25716fecf1e23ff010943c541edf5f2eaa7771c5
unknown <Sophia Nguyen>
2022-03-17 01:56:25 -0500

    added onclick functionality for links in the nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,45 @@
 import React from "react";
 
 function Nav() {
+const navLabels = [
+    {
+        name: "Projects", description: "Here are my work examples",
+    },
+    {
+        name: "Resume", description: "my resume",
+    },
+]; 
+
+function navTitlesSelected(name) {
+    console.log(`${name} clicked`)
+}
     return (
-        <header></header>
+        <header>
+            <h2>
+            <a href="/">
+            Sophia Nguyen Portfolio
+            </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {navLabels.map((navTitles) => (
+                     <li className="mx-1"
+                     key={navTitles.name}
+                     >
+                         <span onClick={navTitlesSelected} >
+                             {navTitles.name}
+                         </span>
+                     </li>   
+                    ))}
+                </ul>
+            </nav>
+        </header>
 
     );
 }

43d139985aba4a9043e722ad07de80dc4cfe6256
unknown <Sophia Nguyen>
2022-03-17 01:32:44 -0500

    fixed document.getelementbyid and not querySelector. image and text shows up now

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import mainImage from "../../assets/me.jpg";
 
 function About() {
     return (
         <section className="my-5">
             <h1 id="about">Sophia Nguyen</h1>
+            <img src={mainImage} className="my-2" style={{width: "25%"}} alt="Sophia Nguyen" />
         </section>
 
     );

e51afbbe777350c887076dbf2ce3ef459a279fbe
unknown <Sophia Nguyen>
2022-03-17 16:22:34 -0500

    added some font-styling changes to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,9 @@ import mainImage from "../../assets/me.jpg";
 function About() {
     return (
         <section className="my-5">
-            <h1 id="about">Sophia Nguyen</h1>
-            <h2>Student at UMN-TC</h2>
-            <img src={mainImage} className="my-2" style={{width: "25%"}} alt="Sophia Nguyen" />
+            <h1 id="about" class="mainTitle">Sophia Nguyen</h1>
+            <h2 class="subtTitle">Student at UMN-TC</h2>
+            <img src={mainImage} className="my-2" style={{width: "15%"}} alt="Sophia Nguyen" />
             <p>
             Passionate and devoted first-generation college student with many technological skills and heavy involvement with non-profit student organizations. Currently transferring universities to pursue a B.A.Sc. degree in Information Technology Infrastructure. Looking to leverage her knowledge and experience into more professional opportunities. 
             </p>

a5de045c43f6de488965f5fd0cfce7cadf849472
unknown <Sophia Nguyen>
2022-03-17 16:40:56 -0500

    added more styling and added usestate for nav

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import photo from "../../assets/coding.jpg"
+import photo from "../../assets/coding.jpg";
+import heroMaps from "../../assets/marvel.jpg";
 
 function Projects() {
     return(
-        <div>
-            <h1 id="projects">Projects</h1>
+        <section class="my-5">
+            <h1 id="projects" className="mainTitle">Projects</h1>
             <img src={photo} alt="coding example"/>
-        </div>
+            <img src={heroMaps} alt="coding example"/>
+        </section>
     );
 }
 

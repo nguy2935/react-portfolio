75c3f21f3798bc6cd93bf94634878f3356781e29
unknown <Sophia Nguyen>
2022-03-18 01:07:42 -0500

    updated projects file

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,8 +6,11 @@ function Projects() {
     return(
         <section class="my-5">
             <h1 id="projects" className="mainTitle">Projects</h1>
+
+            <div className="portfolio-container">
             <img src={photo} alt="coding example"/>
             <img src={heroMaps} alt="coding example"/>
+            </div>
         </section>
     );
 }

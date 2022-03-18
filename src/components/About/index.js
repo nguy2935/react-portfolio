7df2d00045385d834a05d771a6e3c36e16109613
unknown <Sophia Nguyen>
2022-03-18 01:05:57 -0500

    styled resume section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,12 +4,14 @@ import mainImage from "../../assets/me.jpg";
 function About() {
     return (
         <section className="my-5">
+            <div className="aboutInfo">
             <h1 id="about" className="mainTitle">Sophia Nguyen</h1>
             <h2 className="subTitle">Student at UMN-TC</h2>
             <img src={mainImage} className="my-2" style={{width: "15%"}} alt="Sophia Nguyen" />
             <p className="content-text">
             Passionate and devoted first-generation college student with many technological skills and heavy involvement with non-profit student organizations. Currently transferring universities to pursue a B.A.Sc. degree in Information Technology Infrastructure. Looking to leverage her knowledge and experience into more professional opportunities. 
             </p>
+            </div>
         </section>
 
     );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,57 @@ import React from "react";
 
 function Resume() {
     return (
-        <section className="my-5">
-            <h1 id="resume">My Resume</h1>
+        <section id="resume" className="my-5">
+            <h1 className="mainTitle">My Resume</h1>
+            <h2 className="subTitle">My Previous Experiences</h2>
+
+            <div className="resumeContainer">
+                <div className="haveSkills">
+                    <h3>Skills</h3>
+                    <div className="resume-content">
+                    <article className="resume-details">
+                            <p className="details">
+                                <h4>HTML</h4>
+                                blah blah blah
+                            </p>
+                        </article>
+                        <article className="resume-details">
+                            <p className="details">
+                                <h4>CSS</h4>
+                                blah blah blah
+                            </p>
+                        </article>
+                        <article className="resume-details">
+                            <p className="details">
+                                <h4>Javascript</h4>
+                                blah blah blah
+                            </p>
+                        </article>
+                        </div>
+                        </div>
+
+                <div className="previousWork">
+                    <h3>Previous Work</h3>
+                    <div className="resume-content">
+                    <article className="resume-details">
+                            <p className="details">
+                                <h4>Gyu-Kaku</h4>
+                                - Server (current)
+                            </p>
+                        </article>
+                        <article className="resume-details">
+                            <p className="details">
+                                <h4>Benihana</h4>
+                                - Server (previous)
+                            </p>
+                        </article>
+                        <article className="resume-details">
+                            <p className="details">
+                                <h4>MarketSource</h4>
+                                - Wireless Team Member (previous)
+                            </p>
+                        </article>
+            </div></div></div>
         </section>
 
     );
